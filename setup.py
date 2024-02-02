6bc5e9376273b51915199be80b87c3c4754f8f99
Guy Knaan <guyknaan@gmail.com>
2024-02-02 13:20:21 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,15 @@ setuptools.setup(name="shachtav_ml",
                  version="0.2.0",
                  author="Guy Knaan",
                  author_email="guyknaan@gmail.com",
-                 packages=setuptools.find_packages())
+                 packages=setuptools.find_packages(),
+                 install_requires = [
+                     "pretty_midi",
+                     "music21",
+                     "git+https://github.com/CPJKU/madmom.git",
+                     "numpy",
+                     "tensorflow~=2.15.0",
+                     "keras~=2.15.0",
+                     "tqdm",
+                     "librosa",
+                     "pandas"
+                 ])
